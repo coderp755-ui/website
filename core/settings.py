50971b511eaf37439e3ba93c19113e6699463597
coderp755-ui <coderp755@gmail.com>
2026-01-29 15:02:12 +0545

    Added themigartions

--- core/settings.py
+++ core/settings.py
@@ -147,9 +147,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (User uploaded files)
-MEDIA_ROOT = "/opt/render/project/media"
-MEDIA_URL = "/media/"
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
