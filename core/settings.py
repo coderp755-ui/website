9a226c8a72c55c41b7a9ce14edad0bc5227574d0
coderp755-ui <coderp755@gmail.com>
2026-01-29 12:54:59 +0545

    Added themigartions

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
